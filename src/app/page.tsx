7adc20e8e40167e6edf06e5a158a5b4551dec7b1
mohsindev369 <mohsinakrambukhari@gmail.com>
2023-08-20 15:54:12 +0500

    completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
-'use client'
-import { SketchPicker } from 'react-color'
+"use client";
+import React, { useState } from "react";
+import { HexColorPicker } from "react-colorful";
 
+export default function App() {
+  const [color, setColor] = useState("#b32aa9");
 
-export default function Home() {
-  return <SketchPicker />
+  return (
+    <div
+      className={`flex flex-col justify-center items-center h-screen w-full `} style={{background:color}}
+    >
+      <h1 className="font-bold text-4xl mb-4">Color Picker</h1>
+      <div className="bg-white border border-white p-4 rounded-xl">
+
+      <HexColorPicker color={color} onChange={setColor} />
+      </div>
+      {/* <div className="value" style={{ borderLeftColor: color }}>
+        Current color is {color}
+      </div> */}
+      {/* <div className={`h-12 w-12 bg-[${color}]`}></div> */}
+
+      {/* <div className="buttons">
+        <button onClick={() => setColor("#c6ad23")}>Choose gold</button>
+        <button onClick={() => setColor("#556b2f")}>Choose green</button>
+        <button onClick={() => setColor("#207bd7")}>Choose blue</button>
+      </div> */}
+    </div>
+  );
 }
